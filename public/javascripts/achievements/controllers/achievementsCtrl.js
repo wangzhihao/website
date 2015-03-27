0326e305cc314c5771939e6fc76bee85d31ac9a9
wang zhihao <accept.acm@gmail.com>
2015-03-27 10:39:55 +0800

    add color on each icon

--- public/javascripts/achievements/controllers/achievementsCtrl.js
+++ public/javascripts/achievements/controllers/achievementsCtrl.js
@@ -6,7 +6,14 @@
 
   function achieveCtrl(achieveService) {
     var ctrl = this;
-
+    ctrl.colorClasses = [
+      'knowledge_level',
+      'comprehension_level',
+      'application_level',
+      'analysis_level',
+      'synthesis_level',
+      'evaluation_level'
+    ];
     achieveService.getAchievements().then(function(results) {
       //group by subject firstly.
       results = _.groupBy(results, function(item) {
