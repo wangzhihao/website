65171c2b8cdc12c13bbdaa7e89059f04785fddca
wang zhihao <accept.acm@gmail.com>
2015-05-09 07:55:07 +0800

    use the achievements as the index page.

--- app.js
+++ app.js
@@ -25,11 +25,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+/*
 app.use('/', routes);
 app.use('/users', users);
 app.use('/history', history);
 app.use('/achievements', achievements);
 app.use('/stickers', stickers);
+*/
+app.use('/', achievements);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
