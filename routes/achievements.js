920500041157c8be3dbb66ebc288dcd87719eab5
wang zhihao <accept.acm@gmail.com>
2015-05-09 09:31:26 +0800

    change title

--- routes/achievements.js
+++ routes/achievements.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET achievements page. */
 router.get('/', function(req, res, next) {
-  res.render('achievements', { title: 'Achievements' });
+  res.render('achievements', { title: 'Accomplishments' });
 });
 
 module.exports = router;
